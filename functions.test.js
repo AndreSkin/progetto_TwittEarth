e6e094905337e2deb3a63546c17ee58e00d1c6f2
Umberto Carlucci <umberto.carlucci@studio.unibo.it>
2021-11-19 15:51:19 +0100

    Fix server ed import

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const app = require('./server.js');
+const external = require('./server.js');
 const supertest = request = require('supertest')
 
 //const serverUrl = "https://localhost:8000/";
@@ -8,7 +8,7 @@ const serverUrl = "https://site202136.tw.cs.unibo.it/";
 describe("Testing GETs", () => {
     describe("with given hashtag", () => {
       beforeAll(async () => {
-        return 		testHashtags = await request(app).get('/recents/testingtweetuniboswe2122').send({});
+        return testHashtags = await request(external.httpServer).get('/recents/testingtweetuniboswe2122').send({});
       })
       test("Status code is 200", () => {
         expect(testHashtags.statusCode).toEqual(200);
