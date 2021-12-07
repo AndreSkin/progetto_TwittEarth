a08404f67ffd6bcb30496d906022c24c7c4d652a
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2021-12-07 23:20:23 +0100

    Refactoring e commenti

--- server.js
+++ server.js
@@ -1,27 +1,62 @@
+//dotenv per mantenere segreti i token di accesso
 const result=require('dotenv').config({path: `${__dirname}/.env`})
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
+//Twit per la gestione delle API di Twitter v1
 const Twit = require('twit');
+//twitter-api-v2 per la gestione delle API di Twitter v2 e la stream
 const { TwitterApi, ETwitterStreamEvent, TweetStream, ETwitterApiError } = require('twitter-api-v2');
+//multilang-sentiment per la sentiment analysis
 var sentiment = require('multilang-sentiment');
-var stream = ''
+//langdetect per il riconoscimento della lingua deii tweet
 var langdetect = require('langdetect');
-
+//date-and-time per la gestione delle date
 const date_time = require('date-and-time');
-
+//Server per l'applicazione e per la stream
 let httpServer = require("http").createServer(app);
+//xmlhttprequest per richieste http
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
+//path per i percorsi dei file
 const path = require('path');
+//socket.io per far arrivare al front-end i tweet della stream
 const Server = require("socket.io");
 const io = require("socket.io") (httpServer, {cors:{origin:"*"}})
-
+//nodemailer per inviare mail tramite smtp
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport('smtps://' + process.env.MAIL + ':' + process.env.MAIL_PSW + '@smtp.gmail.com');
 
-async function sendmail(from, to, subj, text, html)
-{
+//stream è dichiarato globale per ragioni di scope
+var stream = '';
+
+app.use('/', express.static(__dirname + '/'));
+app.use(cors());
+
+//Token di autenticazione per le API di Twitter
+const b_token= process.env.BEARER_TOKEN;
+
+var T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+})
+
+
+//Istanzio client per twitter API
+const twitterClient = new TwitterApi(b_token);
+const client = twitterClient.readOnly;
+//Client con bearer token academic research
+const twitterClientPRO = new TwitterApi(process.env.PRO_BEARER_TOKEN);
+const clientPRO = twitterClientPRO.readOnly;
+
+app.use(express.json());
+
+/*FUNZIONI*/
+//Funzione per inviare una mail parametrizzata
+async function sendmail(from, to, subj, text, html){
   var mailOptions = {
       from: `'${from} <${process.env.MAIL}>'`, // sender address
       to: `${to}`, // list of receivers
@@ -38,38 +73,139 @@ async function sendmail(from, to, subj, text, html)
 });
 }
 
-//sendmail("prova","team10igsw2021@gmail.com","ciao","Hello world","<b>PROVA<b>");
-
-const b_token= process.env.BEARER_TOKEN;
-
-app.use('/', express.static(__dirname + '/'));
-app.use(cors());
-
-var T = new Twit({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-})
-
-
-//Istanzio client per twitter API
-const twitterClient = new TwitterApi(b_token);
-const client = twitterClient.readOnly;
-
-const twitterClientPRO = new TwitterApi(process.env.PRO_BEARER_TOKEN);
-const clientPRO = twitterClientPRO.readOnly;
-
-app.use(express.json());
-
-/*API*/
-
-//API v2 che dato uno username restituisce la timeline dei suoi Tweets
+//Funzione per embed di tweet
 async function embedTweet(id){
   const tweet = await client.v1.oembedTweet(id);
   return tweet.html;
 }
 
+//Dato un author id restituisce l'username dell'autore corrispondente
+async function getauthor(author_id){
+  let author= '';
+  try{
+    author = await client.v2.user(author_id);
+  }
+  catch(error){
+    console.log("ERROR IN GET AUTHOR", error);
+  }
+  return author.data.username;
+}
+
+//Dato un place id restituisce nome e coordinate corrispondenti
+async function getGeo(place_id){
+  let place= '';
+  try{
+    place = await client.v1.geoPlace(place_id);
+  }
+  catch(error){
+    console.log("ERROR IN GET GEO",error);
+  }
+
+  return {'Name': place.full_name, 'coord_center': place.contained_within[0].centroid};
+}
+
+/*Dato un array di coordinate trova il centro*/
+function find_medium(coordinates){
+  let sumX = 0;
+  let sumY = 0;
+  for (let coord of coordinates){
+    sumX += coord[0];
+    sumY += coord[1];
+  }
+  let mediaX = sumX / coordinates.length;
+  let mediaY = sumY / coordinates.length;
+
+  return [mediaX ,mediaY];
+}
+
+//Funzione di sentiment analysis
+async function sentiment_analyze(toAnalyze){
+  let tweet_eval = {'eval': []};
+  try{
+    //Rimuovo '@' e '#' perchè impediscono una corretta indivduazione della lingua
+    const regExHash = new RegExp('#', "g");
+    const regExTag = new RegExp('@', "g");
+    let notag = toAnalyze.replace(regExHash,'');
+    let replaced = notag.replace(regExTag,'');
+
+    //Riconosco la lingua in cui è scritto il tweet (se non riconosciuta ritorna null e ignora il tweet)
+    var lang = langdetect.detectOne(replaced);
+    let ParsedTweet = await sentiment(replaced, lang);
+
+    //Inserisco i dati dell'analisi sul tweet
+    tweet_eval.eval.push({
+      "Score": ParsedTweet['score'], //Punteggio assegnato
+      "TotL": ParsedTweet['tokens'].length, //Lunghezza del tweet
+      "Pos": ParsedTweet['positive'], //Array di parole identificate come positive
+      "PosL":ParsedTweet['positive'].length,
+      "Neg" : ParsedTweet['negative'], //Array di parole identificate come negative
+      "NegL":ParsedTweet['negative'].length
+    });
+  }
+  catch (e){
+    //In caso di errori (es. tweet non significativi contenenti solo un tag), inserisco i dati a mano
+    //Inserimento di sicurezza dato che per ora l'unico errore è stata la lingua non identificata (Legge di Murphy)
+    tweet_eval.eval.push({
+      "Score": 0,
+      "TotL": 0,
+      "Pos": 0,
+      "PosL":0,
+      "Neg" : 0,
+      "NegL":0
+    });
+  }
+  return(tweet_eval);
+}
+
+//Funzione per cancellare tutte le rules per le stream
+async function delete_rules(){
+  let rules='';
+  let idArray=[];
+  try
+  {
+    rules = await client.v2.streamRules();
+
+    for(let rule of rules.data)
+    {
+      idArray.push(rule.id);
+    }
+
+    await client.v2.updateStreamRules({
+      delete: {
+        ids: idArray
+      }
+    });
+  }
+  catch (e)
+  {
+    console.log("ERROR IN DELETE RULE: ", e);
+  }
+}
+
+//Funzione che dato un luogo ne restituisce le coordinate (API openstreetmap)
+async function getPlace(place) {
+  var xhr = new XMLHttpRequest();
+
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = (e) => {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+
+      if (xhr.status === 200) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        console.warn('request_error');
+      }
+    };
+
+    xhr.open('GET', "https://nominatim.openstreetmap.org/search?format=json&q=%27" + place);
+    xhr.send();
+  });
+}
+
+/*API*/
+//API v2 che dato uno username restituisce la timeline dei suoi Tweets
 app.get('/users/:name', async(req, res) => {
   let userID= '';
   //Dato un nome trovo l'ID
@@ -125,7 +261,9 @@ app.get('/users/:name', async(req, res) => {
         "id": singletweet.id
       })
 
+      //Creo array delle date di creazione dei Tweet
       let formattedDate = singletweet.created_at.substring(0,10);
+      //Se la data non è mai stata inserita la inserisco
       var checkinserted = (element) => element.Date==formattedDate;
       if (!dates.some(checkinserted)){
         dates.push({
@@ -133,6 +271,7 @@ app.get('/users/:name', async(req, res) => {
           "Times":1
         })
       }
+      //Altrimenti aggiorno il contatore delle occorrenze
       else
       {
         for(date of dates)
@@ -151,25 +290,12 @@ app.get('/users/:name', async(req, res) => {
   res.status(200).json({timeline, "Occurrencies":dates});
 });
 
-
-/*Dato un array di coordinate trova il centro*/
-function find_medium(coordinates){
-  let sumX = 0;
-  let sumY = 0;
-  for (let coord of coordinates){
-    sumX += coord[0];
-    sumY += coord[1];
-  }
-  let mediaX = sumX / coordinates.length;
-  let mediaY = sumY / coordinates.length;
-
-  return [mediaX ,mediaY];
-}
-
 /*Tweet vicini a coordinata*/
 app.get('/geo/:place', (req, res) => {
   try{
+    //Ottengo latitudine e longitudine dal parametro della richiesta
     let coord = req.params.place.split("x");
+    //Inizializzo i default dei parametri
     let radius = req.query.radius == undefined? ',10mi': ',' + req.query.radius + 'mi' ;
     let georeq = coord[0] + ',' + coord[1] + radius;
 
@@ -179,7 +305,7 @@ app.get('/geo/:place', (req, res) => {
         res.status(400).json("Nessun tweet trovato")
         return;
       }
-
+      //Preparazione della risposta
       for (let data of data2['statuses']){
         if (data['place'] == null){
           data['geo'] = {'coord_center' : []};
@@ -203,71 +329,6 @@ app.get('/geo/:place', (req, res) => {
     res.status(404).json("error");
   }
 })
-
-
-//Dato un author id restituisce l'username dell'autore corrispondente
-async function getauthor(author_id){
-  let author= '';
-  try{
-    author = await client.v2.user(author_id);
-  }
-  catch(error){
-    console.log("ERROR IN GET AUTHOR", error);
-  }
-  return author.data.username;
-}
-
-//Dato un place id restituisce nome e coordinate corrispondenti
-async function getGeo(place_id){
-  let place= '';
-  try{
-    place = await client.v1.geoPlace(place_id);
-  }
-  catch(error){
-    console.log("ERROR IN GET GEO",error);
-  }
-
-  return {'Name': place.full_name, 'coord_center': place.contained_within[0].centroid};
-}
-
-//Funzione di sentiment analysis
-async function sentiment_analyze(toAnalyze){
-  let tweet_eval = {'eval': []};
-  try{
-    //Rimuovo '@' e '#' perchè impediscono una corretta indivduazione della lingua
-    const regExHash = new RegExp('#', "g");
-    const regExTag = new RegExp('@', "g");
-    let notag = toAnalyze.replace(regExHash,'');
-    let replaced = notag.replace(regExTag,'');
-
-    //Riconosco la lingua in cui è scritto il tweet (se non riconosciuta ritorna null e ignora il tweet)
-    var lang = langdetect.detectOne(replaced);
-    let ParsedTweet = await sentiment(replaced, lang);
-
-    //Inserisco i dati dell'analisi sul tweet
-    tweet_eval.eval.push({
-      "Score": ParsedTweet['score'], //Punteggio assegnato
-      "TotL": ParsedTweet['tokens'].length, //Lunghezza del tweet
-      "Pos": ParsedTweet['positive'], //Array di parole identificate come positive
-      "PosL":ParsedTweet['positive'].length,
-      "Neg" : ParsedTweet['negative'], //Array di parole identificate come negative
-      "NegL":ParsedTweet['negative'].length
-    });
-  }
-  catch (e){
-    //In caso di errori (es. tweet non significativi contenenti solo un tag), inserisco i dati a mano
-    //Inserimento di sicurezza dato che per ora l'unico errore è stata la lingua non identificata (Legge di Murphy)
-    tweet_eval.eval.push({
-      "Score": 0,
-      "TotL": 0,
-      "Pos": 0,
-      "PosL":0,
-      "Neg" : 0,
-      "NegL":0
-    });
-  }
-  return(tweet_eval);
-}
 
 //Trova i tweet contententi la stringa data
 app.get('/recents/:word', async(req, res) => {
@@ -407,77 +468,27 @@ app.get('/recents/:word', async(req, res) => {
   }
 });
 
-
-async function delete_rules(){
-  let rules='';
-  let idArray=[];
-  try
-  {
-    rules = await client.v2.streamRules();
-    //console.log("OLD RULES: ", rules);
-
-    for(let rule of rules.data)
-    {
-      idArray.push(rule.id);
-    }
-
-    await client.v2.updateStreamRules({
-      delete: {
-        ids: idArray
-      }
-    });
-
-    /*
-    rules = await client.v2.streamRules();
-    console.log("RULES AFTER DELETE ", rules.data)
-    */
-  }
-  catch (e)
-  {
-    console.log("ERROR IN DELETE RULE: ", e);
-  }
-}
-
-
-
-async function getPlace(place) {
-  var xhr = new XMLHttpRequest();
-
-  return new Promise((resolve, reject) => {
-
-    xhr.onreadystatechange = (e) => {
-      if (xhr.readyState !== 4) {
-        return;
-      }
-
-      if (xhr.status === 200) {
-        resolve(JSON.parse(xhr.responseText));
-      } else {
-        console.warn('request_error');
-      }
-    };
-
-    xhr.open('GET', "https://nominatim.openstreetmap.org/search?format=json&q=%27" + place);
-    xhr.send();
-  });
-}
-
-
+//API per lo streaming dei tweet di emergenza
+//Chiude lo streaming, se ce ne è uno aperto
 app.get('/stream/closing', async (req, res) => {
   try {stream.close(); res.status(200).json("Closed")} catch { res.status(401).json("Closed too soon")}
 })
 
+//Avvia la stream dei tweet di emergenza (contententi parole associate alle emergenze)
 app.get('/stream/tweets', async (req, res) => {
+//All'inizio prova a chiudere eventuali stream aperte
   try{
     stream.close()
   }
   catch{
     console.log("No already opened stream found")
   }
+
   try
   {
     //await delete_rules();
-    /*const addedRules =*/
+
+    //Aggiunge le regole
      await client.v2.updateStreamRules({
       add: [
     { value: 'SOSigsw10', tag: 'SOS' },
@@ -490,8 +501,10 @@ app.get('/stream/tweets', async (req, res) => {
     ],
   });
 
+  //Avvia la streaming
     stream = await client.v2.searchStream({'expansions':['geo.place_id', 'author_id']});
 
+    //GESTORI DEGLI EVENTI DELLA STREAM
     // Awaits for a tweet
     stream.on(
       // Emitted when Node.js {response} emits a 'error' event (contains its payload).
@@ -510,27 +523,25 @@ app.get('/stream/tweets', async (req, res) => {
       ETwitterStreamEvent.DataKeepAlive,
       () => console.log('Twitter has a keep-alive packet.'),
     );
+
     var emergencytweets=[];
     var coords=[];
     stream.on(
       // Emitted when a Twitter payload (a tweet or not, given the endpoint).
       ETwitterStreamEvent.Data,
       async eventData => {
+        //Emetto evento con socket.io per front-end
         await io.emit('tweet', eventData);
-        //console.log(eventData)
+        //Se il tweet è geolocalizzato
         if (eventData.data.geo.place_id != undefined)
         {
-            //let location = await getGeo(eventData.data.geo.place_id);
+            //Inizializzo i campi
             let author = eventData.includes.users[0]
             let location = eventData.includes.places
             let placecoords =''
             await getPlace(location[0].full_name).then(res => placecoords = res);
 
-            console.log("INCLUDES: ", location)
-            console.log("PLACE: ", placecoords[0])
-            console.log("AUTHOR: ", author)
-
-
+            //Se è stato usato l'hashtag d'emergenza invio subito una mail
             if (eventData.data.text.includes("SOSigsw10"))
             {
               var html = `L'hashtag #SOSigsw10 è stato utilizzato! <hr>
@@ -552,22 +563,24 @@ app.get('/stream/tweets', async (req, res) => {
                 "text":eventData.data.text
               });
               console.log("ET: ", emergencytweets)
+              //Se ho 5 tweet con parole associate alle emergenze
               if (emergencytweets.length == 5)
               {
                 let removed = false;
                 for (var i = 0; i < emergencytweets.length; i++)
                 {
+                  //Prendo come riferimento il primo per le coordinate
                   if ((await Math.abs(emergencytweets[0].lat - emergencytweets[i].lat) > 1) ||
                       (await Math.abs(emergencytweets[0].lon - emergencytweets[i].lon) > 1))
                   {
                     removed=true;
+                    //Se i tweet sono lontani dal primo li rimuovo
                     await emergencytweets.splice(i,1);
                   }
                 }
                 if (removed==false)
                 {
-                  var EText=''
-                  ECoords=''
+                  //Se non ho rimosso tweets significa che sono tutti vicini, quindi invio la mail
                   var EHtml= `Cinque tweet contenenti parole inerenti le emergenze sono stati scritti! <hr>`
 
                   for(ET of emergencytweets)
@@ -601,13 +614,12 @@ app.get('/stream/tweets', async (req, res) => {
 
   io.on("connection", (socket) => {
     console.log('IO connected...')
-    resp = [];
   });
 
   //setTimeout(function(stream){try {stream.close()} catch { res.status(401).json("Closed too soon")}}, 60000, stream);
 });
 
-
+//API per trivia tramite poll
 app.get('/poll/:pollTag', async (req, res) => {
   var PollTweets='';
   try{
@@ -621,6 +633,7 @@ app.get('/poll/:pollTag', async (req, res) => {
       query = '#' + query;
     }
 
+    //Cerco i tweet con poll contententi l'hashtag del trivia e li aggiungo alla risposta
     PollTweets = await client.v2.search(query, {'expansions':['attachments.poll_ids', 'author_id'], 'poll.fields':'duration_minutes,end_datetime,id,options,voting_status'});
     var polls=[];
     var singlepoll='';
@@ -651,7 +664,7 @@ app.get('/poll/:pollTag', async (req, res) => {
     return;
   }
 
-
+  //Cerco il tweet che rivela le risposte corrette e le aggiungo alla risposta
   var answ='';
   try{
 
@@ -675,7 +688,7 @@ app.get('/poll/:pollTag', async (req, res) => {
 res.status(200).json(polls);
 })
 
-
+//API per concorso letterario
 app.get('/concorso/:tagConcorso', async (req, res) => {
   var Bandoconcorso='';
   var Partecipanti='';
@@ -691,11 +704,11 @@ app.get('/concorso/:tagConcorso', async (req, res) => {
     if (query[0] == '~'){ //~ perchè è così che arrivano le richieste dato che # è un carattere vietato
       query = req.params.tagConcorso.substring(1);
     }
-
+    //Cerco il tweet che bandisce il concorso
     Bandoconcorso = await client.v2.search('#'+'bandiscoconcorso' + query, {'max_results':100,'expansions': 'author_id','tweet.fields':'created_at'});
 
     var dataBando = new Date(Bandoconcorso._realData.data[0].created_at);
-    var duration =60;
+    var duration = 60;
     if (Bandoconcorso._realData.data == undefined) {
       res.status(404).json("Nessun tweet trovato")
       return;
@@ -706,7 +719,7 @@ app.get('/concorso/:tagConcorso', async (req, res) => {
       "Banditore":Bandoconcorso._realData.includes.users[0].username
     });
 
-
+    //Cerco tutti quelli che si sono iscritti al concorso tramite hashtag in tempo utile
     Partecipanti = await client.v2.search('#'+'partecipoconcorso' + query, {'max_results':100,'expansions': 'author_id','tweet.fields':'created_at'});
 
     if (Partecipanti._realData.data == undefined) {
@@ -721,6 +734,7 @@ app.get('/concorso/:tagConcorso', async (req, res) => {
     }
   }
 
+    //Cerco i tweet di chi ha votato libri partecipanti tramite hashtag, in tempo utile
     Voters = await client.v2.search('#'+'votoconcorso' + query, {'max_results':100,'expansions': 'author_id','tweet.fields':'created_at'});
 
     if (Voters._realData.data == undefined) {
@@ -747,6 +761,7 @@ app.get('/concorso/:tagConcorso', async (req, res) => {
         }
           var checkvotanti = concorso.votanti.filter(elem => elem.ID==voter.author_id);
 
+          //Controllo che ognuno voti massimo 10 volte, i voti extra annullano i primi in ordine cronologico
           if (checkvotanti.length > 9){
             if (!votato.some(checkvoted)){
               votato.push(voter.author_id);
@@ -758,7 +773,7 @@ app.get('/concorso/:tagConcorso', async (req, res) => {
       }
     }
 
-
+    //Creo il report libri - numero di voti
     for(part of concorso.partecipanti)
     {
       var contavoti=concorso.votanti.filter(elem => elem.Voto==part);
@@ -776,8 +791,5 @@ app.get('/concorso/:tagConcorso', async (req, res) => {
   res.status(200).json(concorso);
 
 })
-
-
-
 
 module.exports.httpServer = httpServer;
